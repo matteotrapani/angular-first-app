6bcfe5bdd89283173b142b9bc159c6123c6216c8
Matteo Trapani <matteo.trapani@ennova-research.com>
2018-03-29 10:05:17 +0200

    improvements

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+const ONLINE: string = 'online';
+const OFFLINE: string = 'offline';
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white
+        }
+    `]
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+    serverId: number = 10;
+    serverStatus: string = OFFLINE;
 
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? ONLINE : OFFLINE;
+    }
+
+    ngOnInit() {
+
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === ONLINE ? 'green' : 'red';
+    }
+
+    isServerOnline() {
+        return this.serverStatus === ONLINE;
+    }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServers2']
 
-  constructor() { }
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = `Server ${this.serverName} was created!`;
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
